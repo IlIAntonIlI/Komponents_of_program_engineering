0b21e026a8222035acb73b043e1ebec076b9b3f1
faaant <illiaeducation@gmail.com>
2021-04-28 22:37:50 +0300

    Add showHelp

--- Cash_register/main.cpp
+++ Cash_register/main.cpp
@@ -31,36 +31,8 @@ int main()
             int flag4;
             cout<<"Write \n0 - how to scan price\n1 - how to create check\n2 - how to end the program"<<endl;
             cin>>flag4;
-            if(flag4==0)
-            {
-                system("cls");
-                cout<<"======               SCANING PRICE OF PRODUCT               ======\n";
-                cout<<"1)Choose \"0\" at the start menu.\n\n2)At the this step scan product:write ID (from 0 to 5) and you can see name of product and it's price.\n";
-            }
-            else if(flag4==1)
-            {
-                system("cls");
-                cout<<endl<<"======               CREATING CHECK               ======\n";
-                cout<<"1)Choose \"1\" at the start menu.\n\n2)At the this step scan product:write ID (from 0 to 5) and this product will be added to the check.\n\n";
-                cout<<"3)If you want to add more products, select \"0\" in the next menu and repeat previous step.\n\n";
-                cout<<"4)If you want to pay for the goods, select \"1\" in the menu described in step 3.\n\n";
-                cout<<"5)In this step, enter the money in the cash register.\n\n";
-                cout<<"    5.1)If entered enough money, your payment has been made successfully and you can take the check.\n\n";
-                cout<<"    5.2)If entered not enough money, you need to add money (choose \"1\") or delete some products from check (choose\"0\").\n\n";
-                cout<<"        5.2.1) If you choose \"1\", your payment has been made successfully and you can take the check.\n\n";
-                cout<<"        5.2.2) If you choose \"0\", write the number of product you want to delete.\n\n";
-
-            }
-            else if(flag4==2)
-            {
-                system("cls");
-                cout<<"======               END THE PROGRAM               ======\n";
-                cout<<"1)Choose \"-1\" in main menu to close the program";
-            }
-            cout<<endl<<endl;
-            system("pause");
+            display1.showHelp(flag4);
             flag=3;
-            system("cls");
         }
         if(flag==3)
         {
@@ -109,7 +81,6 @@ int main()
                         int cash1;
                         cin>>cash1;
                         cash=cash+cash1;
-                        //cout<<endl<<"Thanks for choosing our shop, take your check please."<<endl<<endl; printer.printCheck(casa.check);
                     }
                     else if(flag3==0)
                     {
