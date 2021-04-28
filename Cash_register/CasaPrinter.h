463e81594285e621e73d092a55cad2132e780613
IlIAntonIlI <anton.adamchuk.kpi@gmail.com>
2021-04-28 22:31:19 +0300

    Added "Help"

--- Cash_register/CasaPrinter.h
+++ Cash_register/CasaPrinter.h
@@ -1,7 +1,11 @@
 using namespace std;
 class CasaPrinter{
 public:
-    printCheck(vector<Product> &check){
+    printCheck(vector<Product> &check, int cash){
+        int sum=0;
+        for(int i=0; i<check.size();i++)
+            sum=sum+check[i].price;
+        cout<<"Your balance: "<<cash-sum<<endl;
         check.clear();
     }
 };
--- Cash_register/main.cpp
+++ Cash_register/main.cpp
@@ -19,23 +19,60 @@ int main()
     Database dataBaseProducts;                                                                                                                                                                                                        dataBaseProducts.FillDatabase();
     Display display1;
     CashAcceptDevice device;
-    cout<<"Write: \n 0 - scan price \n 1 - create a check \n-1 - end program"<<endl;
+    Scanner scan;
+    cout<<"Write: \n 0 - scan price \n 1 - create a check \n 2 - help\n-1 - end program"<<endl;
     cin>>flag;
     while(flag>=0)
     {
         int flag3=-1;
+        if(flag==2)
+        {
+            system("cls");
+            int flag4;
+            cout<<"Write \n0 - how to scan price\n1 - how to create check\n2 - how to end the program"<<endl;
+            cin>>flag4;
+            if(flag4==0)
+            {
+                system("cls");
+                cout<<"======               SCANING PRICE OF PRODUCT               ======\n";
+                cout<<"1)Choose \"0\" at the start menu.\n\n2)At the this step scan product:write ID (from 0 to 5) and you can see name of product and it's price.\n";
+            }
+            else if(flag4==1)
+            {
+                system("cls");
+                cout<<endl<<"======               CREATING CHECK               ======\n";
+                cout<<"1)Choose \"1\" at the start menu.\n\n2)At the this step scan product:write ID (from 0 to 5) and this product will be added to the check.\n\n";
+                cout<<"3)If you want to add more products, select \"0\" in the next menu and repeat previous step.\n\n";
+                cout<<"4)If you want to pay for the goods, select \"1\" in the menu described in step 3.\n\n";
+                cout<<"5)In this step, enter the money in the cash register.\n\n";
+                cout<<"    5.1)If entered enough money, your payment has been made successfully and you can take the check.\n\n";
+                cout<<"    5.2)If entered not enough money, you need to add money (choose \"1\") or delete some products from check (choose\"0\").\n\n";
+                cout<<"        5.2.1) If you choose \"1\", your payment has been made successfully and you can take the check.\n\n";
+                cout<<"        5.2.2) If you choose \"0\", write the number of product you want to delete.\n\n";
 
+            }
+            else if(flag4==2)
+            {
+                system("cls");
+                cout<<"======               END THE PROGRAM               ======\n";
+                cout<<"1)Choose \"-1\" in main menu to close the program";
+            }
+            cout<<endl<<endl;
+            system("pause");
+            flag=3;
+            system("cls");
+        }
         if(flag==3)
         {
             system("cls");
-            cout<<"Write: \n 0 - scan price \n 1 - create a check \n-1 - end program"<<endl;
+            cout<<"Write: \n 0 - scan price \n 1 - create a check \n 2 - help\n-1 - end program"<<endl;
             cin>>flag;
         }
         if(flag==-1) break;
-        Scanner scan;
-        code=scan.readCode();
+
         if(flag==0)
         {
+            code=scan.readCode();
             system("cls");
             cout<<"The price of "<<dataBaseProducts.getNameProduct(code)<<": "<<dataBaseProducts.getPrice(code)<<" UAH"<<endl<<endl;
             cout<<"Write: \n 0 - scan price \n 1 - create a check \n-1 - end program"<<endl;
@@ -44,6 +81,7 @@ int main()
         else if (flag==1)
         {
             system("cls");
+            code=scan.readCode();
             casa.addProductToCheck(code,dataBaseProducts,display1);
             int flag1=0;
             cout<<"Write:\n1 - pay for products and print check \n0 - continue scan products"<<endl;
@@ -62,7 +100,17 @@ int main()
                     display1.showCheck(casa.check);
                     display1.noEnoughMoney();
                     cin>>flag3;
-                    if(flag3==1){system("cls");cout<<endl<<"Thanks for choosing our shop, take your check please."<<endl<<endl; printer.printCheck(casa.check); }
+                    if(flag3==1)
+                    {
+                        system("cls");
+                        cout<<"Entered money: "<<cash<<endl;
+                        display1.showCheck(casa.check);
+                        cout<<"Enter money: ";
+                        int cash1;
+                        cin>>cash1;
+                        cash=cash+cash1;
+                        //cout<<endl<<"Thanks for choosing our shop, take your check please."<<endl<<endl; printer.printCheck(casa.check);
+                    }
                     else if(flag3==0)
                     {
                         system("cls");
@@ -75,7 +123,7 @@ int main()
                         system("cls");
                     }
                 }
-                if (!casa.check.empty()){system("cls");cout<<"Thanks for choosing our shop, take your check please."<<endl<<endl;printer.printCheck(casa.check);flag3=1;}
+                if (!casa.check.empty()){system("cls");cout<<"Thanks for choosing our shop, take your check please."<<endl;printer.printCheck(casa.check,cash);cout<<endl;flag3=1;}
             }
         }
         if ((casa.check.empty()) &&(flag3!=-1))
